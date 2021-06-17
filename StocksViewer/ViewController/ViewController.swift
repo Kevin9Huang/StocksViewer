ace20ffe140aba9d96f3af49b5c523aa2fdf025d
Kevin Huang <kevin9huang1994@gmail.com>
2021-06-17 18:16:55 +0700

    Deleted main storyboard, added folders

---
+++ StocksViewer/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StocksViewController.swift
+//  StocksViewer
+//
+//  Created by Kevin Huang on 17/06/21.
+//
+
+import UIKit
+
+class StocksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
